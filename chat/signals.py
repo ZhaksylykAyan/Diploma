9f2e60df044a0fa8a84b83cf28d4d4defaca133b
Ayan <ay_zhaksylyk@kbtu.kz>
2025-05-26 06:37:31 +0500

    chat status

--- chat/signals.py
+++
@@ -1,9 +0,0 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from django.conf import settings
-from chat.models import UserStatus
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_user_status(sender, instance, created, **kwargs):
-    if created:
-        UserStatus.objects.get_or_create(user=instance)
